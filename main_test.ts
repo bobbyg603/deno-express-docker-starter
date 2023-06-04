a92c20b2f9cc058bae3de6203e0c336016f3490d
Bobby Galli <bobbyg603@gmail.com>
2023-06-04 13:00:25 -0400

    chore: add test and bench

---
+++ main_test.ts
@@ -0,0 +1,16 @@
+import { assertEquals } from "https://deno.land/std@0.182.0/testing/asserts.ts";
+const stagingUrl = Deno.env.get("STAGING_URL") || "http://localhost:5090";
+
+Deno.test("Plural Users Route", async () => {
+    const res = await fetch(`${stagingUrl}/users`);
+    const response = await res.json();
+    console.log(response);
+    assertEquals(res.status, 200);
+});
+
+Deno.test("Single User Route & User Data", async () => {
+    const res = await fetch(`${stagingUrl}/users/2`);
+    const response = await res.json();
+    assertEquals(res.status, 200);
+    assertEquals(response.name, "Funmi");
+});
