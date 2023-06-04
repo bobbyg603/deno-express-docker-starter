fd46880de4ad812fe269eeb83e52076312d131b1
Bobby Galli <bobbyg603@gmail.com>
2023-06-04 12:17:44 -0400

    chore: add basic Dockerfile

---
+++ deps.ts
@@ -0,0 +1,5 @@
+// @deno-types="npm:@types/express@4"
+import express, { NextFunction, Request, Response } from "npm:express@4.18.2";
+
+export { express };
+export type { NextFunction, Request, Response };
---
+++ main.ts
@@ -0,0 +1,34 @@
+import { Request, Response, NextFunction, express } from "./deps.ts";
+
+const app = express();
+const port = Number(Deno.env.get("PORT")) || 5090;
+const demoData = JSON.parse(Deno.readTextFileSync('./demo.json'));
+
+const reqLogger = function (req: Request, _res: Response, next: NextFunction) {
+    console.info(`${req.method} request to "${req.url}" by ${req.hostname}`);
+    next();
+};
+
+app.use(reqLogger);
+
+app.get("/", (_req, res) => {
+    res.status(200).send("Hello from Deno and Express!");
+});
+
+app.get("/users", (_req, res) => {
+    res.status(200).json(demoData.users);
+});
+
+app.get("/users/:id", (req, res) => {
+    const idx = Number(req.params.id);
+    for (const user of demoData.users) {
+        if (user.id === idx) {
+            res.status(200).json(user);
+        }
+    }
+    res.status(400).json({ msg: "User not found" });
+});
+
+app.listen(port, () => {
+    console.log(`Listening on ${port} ...`);
+});
